599d245f4c9492eecd3f6c2da1b7727f90b72caa
juhapekkaylisela <juha-pekka.ylisela@clickon.co>
2022-02-14 17:37:00 +0000

    remove two week option

--- src/Views.js
+++ src/Views.js
@@ -2,7 +2,6 @@ import { views } from './utils/constants'
 import Month from './Month'
 import Day from './Day'
 import Week from './Week'
-import TwoWeek from './TwoWeek'
 import WorkWeek from './WorkWeek'
 import Agenda from './Agenda'
 import Gantt from './Gantt'
@@ -10,7 +9,6 @@ import Gantt from './Gantt'
 const VIEWS = {
   [views.MONTH]: Month,
   [views.WEEK]: Week,
-  [views.TWO_WEEK]: TwoWeek,  
   [views.WORK_WEEK]: WorkWeek,
   [views.DAY]: Day,
   [views.AGENDA]: Agenda,
